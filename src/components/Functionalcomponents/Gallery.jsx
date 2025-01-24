d5011e2962fd44d61cec7502a78fd1d93b4f1235
Anushiya <anushiya.kokila@gmail.com>
2025-01-24 10:19:37 +0530

    refactoring changes

--- src/components/Functionalcomponents/Gallery.jsx
+++ src/components/Functionalcomponents/Gallery.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const Gallery = (props) => {
   return (
     <div>
       <h4>welcome to {props.page} Page</h4>
       <img src="logo.png"alt="Logo"  />  
-        <h2>the image is{props.img}</h2>
+        <h2>the image is {props.img}</h2>
     </div>
   )
 }
